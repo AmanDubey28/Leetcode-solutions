8184900febc34198396881c53b2c22f267d00442
Aman Shailesh Dubey <113586030+AmanDubey28@users.noreply.github.com>
2026-05-08 17:48:38 +0530

    Time: 91 ms (5.82%), Space: 46.3 MB (6.96%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false; //negative cannot be palindrome
+        int rev=0;
+        int original=x;
+        while(x>0){
+            int digit=x%10;
+            rev=rev*10+digit;
+            x=x/10;
+        }
+        return rev==original;
+        
+    }
+}
